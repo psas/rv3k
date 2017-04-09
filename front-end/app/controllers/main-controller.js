c24e8f7ea5e906d692ec8e78ecd36425079265e7
murphpdx <amurphy@localhost.localdomain>
2017-04-09 16:01:43 -0700

    nav bar

--- front-end/app/controllers/main-controller.js
+++ front-end/app/controllers/main-controller.js
@@ -2,7 +2,6 @@
 //This is the main controller, it handles the logic in on the main page (app/index.html)
 angular.module('rvtk', [])
     .controller('MainController', ['$scope', function($scope) {
-    console.log("init");
 
     //initialize variables
     $scope.VisibleCamera = 1;
@@ -13,7 +12,6 @@ angular.module('rvtk', [])
 	//to change the number of feeds to toggle through change the number of feeds variable
 	var numberOfFeeds = 2;
         $scope.VisibleCamera = ++$scope.VisibleCamera%numberOfFeeds;
-	console.log($scope.VisibleCamera);
     }
 }]);
 
