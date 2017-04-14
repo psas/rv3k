6372d81eb753bc9a77e94cec17970a805e106b7f
YunCongChen <yuncong@pdx.edu>
2017-04-14 00:18:42 -0700

    update server.py and telemetry.py

--- back-end/server.py
+++ back-end/server.py
@@ -1,45 +1,42 @@
-#!/usr/bin/env python
-import eventlet
-import eventlet.wsgi
-from flask import Flask, render_template
-import socketio
-import sys
-from telemetry import Telemetry
-from threading import Event, Thread
+async_mode = None
 
-sio = socketio.Server(logger=True, async_mode=None)
+import time
+from flask import Flask, render_template
+from psas_packet import io
+import socket
+import socketio
+import telemetry as tel
+
+sio = socketio.Server(logger=True, async_mode=async_mode)
 app = Flask(__name__)
 app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
-app.config["SECRET_KEY"] = "secret!"
+app.config['SECRET_KEY'] = 'secret!'
 thread = None
 
 def background_thread():
-    event = Event()
-    tel = Telemetry(sio)
-    tel_thread = Thread(target=tel.listen, args=(event,))
-    tel_thread.start()
-#    while True:
-#        try:
-#            pass
-#        except KeyboardInterrupt:
-#            event.set()
-#            tel_thread.join()
-#            sys.exit()
+    """Example of how to send server generated events to clients."""
+    telemetry = tel.Telemetry()
+    telemetry.run(sio)
 
 @app.route('/')
 def index():
     global thread
     if thread is None:
         thread = sio.start_background_task(background_thread)
-    return render_template("index.html")
+    return render_template('index.html')
 
-@sio.on("connect", namespace="/main")
-def connect(sid, environ):
-    print("Client connected")
+@sio.on('connect', namespace='/test')
+def test_connect(sid, environ):
+    print('Client connected')
 
-@sio.on("disconnect", namespace="/main")
-def disconnect(sid):
-    print("Client disconnected")
+@sio.on('disconnect', namespace='/test')
+def test_disconnect(sid):
+    print('Client disconnected')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # deploy with eventlet
+    import eventlet
+    import eventlet.wsgi
     eventlet.wsgi.server(eventlet.listen(('', 8080)), app)
+
+
--- back-end/telemetry.py
+++ back-end/telemetry.py
@@ -1,20 +1,20 @@
-from psas_packet import io
-import socket
 import socketio
-from threading import Event
+import socket
+from psas_packet import io
 
 class Telemetry:
-    def __init__(self, sio):
-        self.sio = sio
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind(("127.0.0.1", 35001))
-        self.network = io.Network(self.socket)
-    
-    def listen(self, event):
-        while event.is_set() == False:
-            for timestamp, data in self.network.listen():
-                fourcc, data = data
-                data["recv"] = timestamp
-                self.sio.emit("telemetry", {fourcc: data}, namespace="/main")
-            self.sio.sleep(0.001)
-        return
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('127.0.0.1', 35001))
+        self.net = io.Network(self.sock)
+
+
+    def run(self, sio):
+        sequence = 90501
+        while sequence < 93251:
+            for d in self.net.listen():
+                timestamp, values = d
+                fourcc, values = values
+                sio.emit('my response', {fourcc : dict({"recv" : timestamp}, **values)}, namespace='/test')
+                sio.sleep(0.100)
+                sequence += 1
