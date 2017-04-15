4665cc38dce7510fe27bc1a71600d0c3921b7c15
MatthewTighe <matthewdtighe@gmail.com>
2017-04-15 11:17:38 -0700

    Added comments and removed unnecessary html file.

--- front-end/app/directives/attitude.js
+++ front-end/app/directives/attitude.js
@@ -4,23 +4,24 @@ angular.module("rvtk").directive("attitude", function() {
     return {
         restrict: 'E',
         scope: {},
-        templateUrl: 'directives/attitude.html',
+        templateUrl: '', 
         link: function($scope, elem, attrs) {
+            // Set up variables needed for the scene.
             var camera;
             var scene;
             var renderer;
             var mesh;
-            var gyros = [];
+            var gyros = [];     // Gyroscopic data handling rotation. 
             $.ajax({
               type: "GET",
-              url: "directives/components/ADIS.csv", <!-- TODO this will be streamed data -->
+              url: "directives/components/ADIS.csv", // TODO: Real data. 
               dataType: "text",
               success: function(data) {
                 process(data);
-                //allThis();
               }
             });
 
+            // Get gyroscopic data out of ADIS.csv.
             function process(text){ 
               var lines = text.split("\n");
               for(var i = 1; i < lines.length-1; i++) {
@@ -33,6 +34,8 @@ angular.module("rvtk").directive("attitude", function() {
             init();
             var loader1 = new THREE.JSONLoader();
 
+            // Load the rocket model into the mesh variable.
+            // The inner function is not called until loader1.load completes.
             function loadModel(modelUrl) {
                 loader1.load(modelUrl, function(geometry) {
                     mesh = new THREE.Mesh(geometry);
@@ -43,6 +46,8 @@ angular.module("rvtk").directive("attitude", function() {
             loadModel("directives/components/rocket_model2.js");
             animate();
 
+            // Set up the camera and renderer and attach them to html.
+            // Start a listener for window resizing.
             function init() {
                 camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, .1, 2000);
                 camera.position.set(0, 0, 10);
@@ -54,15 +59,18 @@ angular.module("rvtk").directive("attitude", function() {
                 window.addEventListener('resize', onWindowResize, false);
             }
 
+            // Resize the renderer when the window resizes.
             function onWindowResize(event) {
                 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
             }
 
+            // Animate the scene.
             function animate() {
                 requestAnimationFrame(animate);
                 render();
             }
 
+            // Render the scene, and change the rocket's attitude every .1 seconds.
             var i = 0;
             function render() {
               setTimeout(function() {
