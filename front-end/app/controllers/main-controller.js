5853e386e95cb370898c62a2743675b2e62529fc
Matthew Tighe <matthewdtighe@gmail.com>
2017-04-17 17:14:30 -0700

    Updated files with license, and changed licenses where necessary.

--- front-end/app/controllers/main-controller.js
+++ front-end/app/controllers/main-controller.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva, Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen, Michael Ohl, Matthew Tighe
+ * ALL RIGHTS RESERVED
+ * [This program is licensed under the "GNU General Public License"]
+ * Please see the file COPYING in the source
+ * distribution of this software for license terms.
+ */
 
 //This is the main controller, it handles the logic in on the main page (app/index.html)
 var app = angular.module('rvtk', []);
--- front-end/app/e2e-tests/scenarios.js
+++ front-end/app/e2e-tests/scenarios.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva, Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen, Michael Ohl, Matthew Tighe
+ * ALL RIGHTS RESERVED
+ * [This program is licensed under the "GNU General Public License"]
+ * Please see the file COPYING in the source
+ * distribution of this software for license terms.
+ */
+
 'use strict';
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
