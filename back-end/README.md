62dd5d474de218e1b8767d8c6d16a661611b7642
Patrick Overton <poverton@pdx.edu>
2017-04-21 15:34:18 -0700

    Added argparse support to server.py (Seth and I produced this during our collab).

--- back-end/README.md
+++ back-end/README.md
@@ -1,9 +1,21 @@
 # Back End Preliminary Directory
-1. mkvirtualenv back-end -p /usr/bin/python3.5
-2. pip install eventlet
-3. pip install flask
-4. pip install psas_packet
-5. pip install python-socketio
-6. python server.py
-7. Navigate to http://0.0.0.0:8080 in your browser
-8. Replay the Launch-12 data
+This directory contains the Socket.IO server and supporting server modules for rv3k.
+
+## Requirements
+- Python 3.5+
+
+## Setup
+1. Clone the rv3k repository to a convenient working directory. (ex: `git clone https://github.com/patter5/rv3k.git`)
+2. Navigate to the back-end directory. (ex: `cd <path-to>/rv3k/back-end`)
+3. Install the dependencies specified in requirements.txt. (ex: `pip install -r requirements.txt`)
+
+### Running the Rocket View 3000 Socket.IO server
+APRS Mode: `python server.py -A`
+Telemetry Mode: `python server.py -T`
+Video Mode: `python server.py -V`
+
+### Testing notes
+Including the flag `-t` tells server.py to serve the testing webpage.
+APRS Testing Address: http://0.0.0.0:8081
+Telemetry Testing Address: http://0.0.0.0:8080
+Video Testing Address: http://0.0.0.0:8082
--- back-end/server.py
+++ back-end/server.py
@@ -6,75 +6,81 @@
 # [This program is licensed under the "GNU General Public License"]
 # Please see the file COPYING in the source distribution of this
 # software for license terms.
+from aprs import AprsReceiver
+from argparse import ArgumentParser
 import eventlet
 import eventlet.wsgi
 from flask import Flask, render_template
-from optparse import OptionParser
-import random
 import socketio
-import sys
+from telemetry import Telemetry
 
 # Create and configure the server socket
 sio = socketio.Server(logger=True, async_mode=None)
 app = Flask(__name__)
 app.wsgi_app = socketio.Middleware(sio, app.wsgi_app)
 app.config["SECRET_KEY"] = "secret!"
+port = None
+server = None
 thread = None
-
 
 # foo() is a function that was created to test how well multiple servers
 # can communicate with a single front end client.
-def foo(number):
-    count = 0
-    while True:
-        sio.emit("foo", {"data": count}, namespace="/main")
-        sio.sleep(random.random())
-        count += 1
-
+#def foo(number):
+#    count = 0
+#    while True:
+#        sio.emit("foo", {"data": count}, namespace="/main")
+#        sio.sleep(random.random())
+#        count += 1
 
 @app.route('/')
 def index():
     global thread
     if thread is None:
-        thread = sio.start_background_task(foo, 0)
+        thread = sio.start_background_task(server.listen)
     return render_template("index.html")
-
 
 # Connection handler for incoming client connections
 @sio.on("connect", namespace="/main")
 def connect(sid, environ):
     print("Client connected")
 
-
 # Connection handler for disconnecting client connections
 @sio.on("disconnect", namespace="/main")
 def disconnect(sid):
     print("Client disconnected")
 
-
 # main() takes a number and a port as command line arguments
 # and based on the arguments from the command line will create a
 # back ground thread that runs the foo()
-def main(argv):
-    global app
+def main():
     # Adding command line option support
-    parser = OptionParser()
-    parser.add_option("-n", "--number", dest="number", action="store",
-                      type="int")
-    parser.add_option("-p", "--port", dest="port", action="store", type="int")
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--test", action="store_true")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-A", "--aprs", action="store_true")
+    group.add_argument("-T", "--telemetry", action="store_true")
+    group.add_argument("-V", "--video", action="store_true")
 
     # Parse command line arguments
-    (options, argv) = parser.parse_args(argv)
+    args = parser.parse_args()
 
-    if options.number > 0:
-        global thread
+    global port, server
+    if args.aprs:
+        port = 8081
+        server = AprsReceiver("127.0.0.1", 35002, sio)
+    elif args.telemetry:
+        port = 8080
+        server = Telemetry("127.0.0.1", 35001, sio)
+    else:
+        pass # Instantiate video server here!
+    if args.test == False:
+        global app, thread
         app = socketio.Middleware(sio)
-        thread = sio.start_background_task(foo, options.number)
+        thread = sio.start_background_task(server.listen)
 
     # Sets the server to listen on a specific port for incoming connections
-    eventlet.wsgi.server(eventlet.listen(('', options.port)), app)
-
+    eventlet.wsgi.server(eventlet.listen(('', port)), app)
 
 # Runs main()
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
