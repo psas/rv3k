d077ec2907b4b12f25a9f4b235afb87c28776ab4
paolo215 <paolo_villanueva215@yahoo.com>
2017-04-21 00:24:06 -0400

    Added copyright notice

--- back-end/aprs_source_input.py
+++ back-end/aprs_source_input.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva,
+# Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen, Michael
+# Ohl, Matthew Tighe
+# ALL RIGHTS RESERVED
+# [This program is licensed under the "GNU General Public License"]
+# Please see the file COPYING in the source distribution of this
+# software for license terms.
+
 import socket
 import time
 
@@ -18,7 +26,6 @@ class AprsSourceInput:
                 sock.send(raw_aprs)
             time.sleep(1)
             i += 1
-
         sock.close()
 
 
