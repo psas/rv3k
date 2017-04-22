b5272bed8ae8513f270347545382bcdc7b076294
Connor Picken <cpicken@pdx.edu>
2017-04-21 19:59:40 -0700

    Added some comments to the file. Should be slightly easier to read, still need to work on making the code more readable.

--- front-end/app/directives/attitude.js
+++ front-end/app/directives/attitude.js
@@ -15,7 +15,7 @@ angular.module("rvtk").directive("attitude", function() {
             var time = [];	  // time stamps from the data packets
             $.ajax({
                 type: "GET",
-                url: "assets/ADIS.csv", // TODO: Real data. 
+                url: "assets/ADIS.csv", 
                 dataType: "text",
                 success: function(data) {
                     process(data);
@@ -41,6 +41,10 @@ angular.module("rvtk").directive("attitude", function() {
 		console.log(imuData[0]);
             }
 
+	    // TODO: For some reason the imuData variable seems to fall out of scope and become undefined after the
+	    // call to process(). I don't know why this behaviour is this way, but having the rest of the code execute
+	    // within a function that is called in the $.ajax(... section fixes this scoping issue.
+	    // In the future fix the styling of this code or find another solution to the scope issue.
             function allThis(){
             init();
             var loader1 = new THREE.JSONLoader();
@@ -70,24 +74,18 @@ angular.module("rvtk").directive("attitude", function() {
                 //window.addEventListener('resize', onWindowResize, false);
             }
 
-            // TODO: apparently according to Matt this may not be necessary. Consider removing in future
+            // TODO: apparently according to Matt this may not be necessary. Consider removing this in the future
             // Resize the renderer when the window resizes.
             function onWindowResize(event) {
                 renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
             }
 
-		/*
-            // Animate the scene.
-            function animate() {
-                requestAnimationFrame(animate);
-                render();
-            }
-		*/
-            var i = 0;
+            var i = 0;			// index for line number in the csv data file
             var n = 0;			// just a loop variable for testing, won't be needed when testing is done
             var dt = 0.0012;		// change in time between data packets
-            var loopDelay = dt * Math.pow(10, 3);	// loop delays needs to be in seconds
+            var loopDelay = dt * Math.pow(10, 3);	// loop delays needs to be in milliseconds
             var firstData = true;	// track whether this is the first data packet we have received
+					// for purposes of not using an invalid dt value on first calculation
             // target rotation values for the rocket model
             var targetRotX = 0;
             var targetRotY = 0;
@@ -105,47 +103,74 @@ angular.module("rvtk").directive("attitude", function() {
                 }, 1000/30);	// run at 30fps
             }
 
+            //This function will process a data packet and update the three targetRot variables
+	    //The 
             function calculateData(){
                 var x = imuData[i];
                 var imuPitch = 0;
                 var imuYaw = 0;
                 
+		// Gyroscope data is in degrees/second. Need to convert to radians.
+		// Data is thus an angular displacement instead of an absolute angle or rotation,
+		// so data is added to target rotation instead of assigning the rotation.
+		// For some reason it appears that the y axis values are actually in the x axis column
+		// so I am switching them here. 
+		// TODO: Currently do not know if x is really x axis or z axis because of the y values
+		// being in the x column. Need to investigate further
                 targetRotX += Math.radians(x[1]) * dt;
                 targetRotY += Math.radians(x[0]) * dt;
                 targetRotZ += Math.radians(x[2]) * dt;
 
+		// Uses data from the accelerometer to obtain a pitch and yaw angle. Note that accelerometer
+		// cannot track roll.
                 imuPitch = Math.atan2(x[5], x[3]);
                 imuYaw = Math.atan2(x[4], x[3]);
 
+		// Combine the gyroscope data with the accelerometer data to ensure that the orientation is both
+		// accurate and free of any drift errors that are present with using gyroscopes.
+		// TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis. 
+		// This would get fixed by using magnometer data, however, initial search into these calculations
+		// seems exceedingly complex and I am not if it is worth the effort. 
                 targetRotX = (0.98 * targetRotX) + (0.02 * imuPitch);
                 targetRotZ = (0.98 * targetRotZ) + (0.02 * imuYaw);
             }
 
             
-
+	    // Function which is used to loop through the line of data in the ADIS.csv file (the contents of which
+            // are now currently in imuData with i being the current index for what line we are currently on).
+            // This function was added so that this program can go through data packets at the rate that the packets
+	    // came in at, so that its rate is independant of how fast the model is being rendered.
+	    // TODO: This function will need to be changed in the future to receive data packets from the server instead
+	    // of looping through a file.
             function loop(){
                 setTimeout(function(){
                     calculateData();
                     if(firstData == true){
+			// TODO: This check should be used to initialize the target rotation with accelerometer data instead
+			// of using a default value of 0, 0, 0.
                         firstData = false;
                     }
                     else{
                         dt = time[i] - time[i - 1];
-                        dt = dt * Math.pow(10, -9); //need to convert nanoseconds to seconds
+                        dt = dt * Math.pow(10, -9); // need to convert nanoseconds to seconds
                     }
                     i += 1;
+                    // TODO: Just commenting out this bit of debug logging code for the time being...
+                    /*
                     n += 1;
                     if(n >= 10){ // just trying to not flood the console while testing
                         console.log(targetRotX);
                         n = 0;
                     }
+                    */
                     if(i == imuData.length - 1){ // in order to loop when at the end of the ADIS.csv file
                         i = 0;
                         firstData = true;
                     }
                     loopDelay = dt * Math.pow(10, 3); // convert seconds to milliseconds since delay time is in milliseconds
                     loop();
-                }, loopDelay);
+                }, loopDelay);	// loopDelay is calculated by the time between current data packet and last data packet.
+				// This is done to try to simulate the rate at which the data packets came in during the live launch
             }
 
             loop();
