5268dde62a426cc208fad70783b66048bdfd5e6c
paolo215 <paolo_villanueva215@yahoo.com>
2017-04-26 22:17:41 -0400

    Experimenting scheduler

--- back-end/telemetry.py
+++ back-end/telemetry.py
@@ -7,13 +7,20 @@
 # software for license terms.
 from psas_packet import io
 import socket
+import sched
+import time
+import threading
 
 class Telemetry:
     def __init__(self, address, port, sio):
         self.address = address
         self.port = port
         self.sio = sio
+        self.data = {}
     
+    def sender(self):
+        pass 
+
     def listen(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock.bind((self.address, self.port))
@@ -23,8 +30,22 @@ class Telemetry:
                 for timestamp, data in network.listen():
                     fourcc, data = data
                     data["recv"] = timestamp
-                    self.sio.emit("telemetry", {fourcc: data},
-                                  namespace="/main")
+                    #self.sio.emit("telemetry", {fourcc: data},
+                    #              namespace="/main")
+                    if not fourcc in self.data:
+                        self.data[fourcc] = {}
+                        for fourcc_data in data:
+                            self.data[fourcc][fourcc_data] = 0
+                            self.data[fourcc]["counter"] = 0
+
+                    for fourcc_data in data:
+                        if isinstance(data[fourcc_data], int) or  \
+                            isinstance(data[fourcc_data], float):
+                            self.data[fourcc][fourcc_data] += data[fourcc_data]
+                            self.data[fourcc]["counter"] += 1
+                        else:
+                            self.data[fourcc][fourcc_data] = data[fourcc_data]
+
                 self.sio.sleep(0.001)
             except KeyboardInterrupt:
                 sock.close()
