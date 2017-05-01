13f87c6765ea2828cf0273fddca10bef6e6f32e0
murphpdx <amurphy@localhost.localdomain>
2017-05-01 00:38:37 -0700

    unit tests

---
+++ front-end/app/components/mainController_test.js
@@ -0,0 +1,38 @@
+
+/**
+ * Copyright (c) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva, Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen, Michael Ohl, Matthew Tighe
+ * ALL RIGHTS RESERVED
+ * [This program is licensed under the "GNU General Public License"]
+ * Please see the file COPYING in the source
+ * distribution of this software for license terms.
+**/
+
+'use strict';
+
+describe('rvtk MainControlller', function() {
+    var $scope, $controller;
+
+    beforeEach(angular.mock.module('rvtk'));
+    beforeEach(inject(function(_$rootScope_, _$controller_) {
+        $scope = _$rootScope_.$new();
+        $controller = _$controller_;
+
+    }));
+
+    describe('toggleCamera', function() {
+        
+        it('should start at one', function() {
+            var controller = $controller('MainController', {$scope: $scope});
+            expect($scope.VisibleCamera).toEqual(1);
+        });
+        it('should increment the feed', function() {
+            var controller = $controller('MainController', {$scope: $scope});
+            $scope.toggleCamera();
+            expect($scope.VisibleCamera).toEqual(0);
+            expect(1).toEqual(0);
+        });
+    });
+
+});
+
+
--- front-end/karma.conf.js
+++ front-end/karma.conf.js
@@ -1,34 +1,53 @@
 //jshint strict: false
 module.exports = function(config) {
-  config.set({
+    config.set({
 
-    basePath: './app',
+        basePath: './app',
 
-    files: [
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'components/**/*.js',
-      'view*/**/*.js'
-    ],
+        // generates the coverage
+        reporters: ['progress', 'coverage'],
 
-    autoWatch: true,
+        preprocessors: {
+            // source files, that you want to generate coverage for
+            'controllers/*.js': ['coverage']
+        },
 
-    frameworks: ['jasmine'],
+        files: [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'components/*.js',
+            'app.js',
+            'controllers/*.js',
+            'controllers/main-controller.js'
+        ],
 
-    browsers: ['Chrome'],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter',
+            'karma-coverage'
+        ],
 
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-firefox-launcher',
-      'karma-jasmine',
-      'karma-junit-reporter'
-    ],
+        //enables colors in the output
+        colors: true,
 
-    junitReporter: {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+        frameworks: ['jasmine'],
 
-  });
+        browsers: ['Chrome'],
+
+        junitReporter: {
+            outputFile: 'test/unit/unit.xml',
+            suite: 'unit'
+        },
+
+        // configure the reporter
+        // creates an html file in the shows the code coverage
+        // the file is located where dir specifies 
+        coverageReporter: {
+            type: 'html',
+            dir: 'tests/coverage'
+        }
+    });
 };
