309a2d2b8337f8ad0f2a23312fb2b67045d92fd0
Connor Picken <cpicken@pdx.edu>
2017-05-05 21:44:35 -0700

    Cleaned up the file by removing some old commented out code.

--- front-end/app/directives/attitude.js
+++ front-end/app/directives/attitude.js
@@ -49,7 +49,6 @@ angular.module("rvtk").directive("attitude", function() {
 
                         $scope.TimeStamp = data[key].timestamp;
                         $scope.calculateData();
-                        //$scope.allThis();
                     }
                 }
             });
@@ -82,9 +81,7 @@ angular.module("rvtk").directive("attitude", function() {
                 $element[0].appendChild($scope.renderer.domElement);
                 //$scope.attitudeGraphic = $scope.renderer.domElement;
                 //$scope.$apply();
-                //
 
-                //window.addEventListener('resize', onWindowResize, false);
             }
 
             $scope.init();
@@ -144,10 +141,6 @@ angular.module("rvtk").directive("attitude", function() {
             }
 
             $scope.deltaTime = 0.0012;	// change in time between data packets
-            $scope.loopDelay = $scope.deltaTime * Math.pow(10, 3); // loop delays needs to be in milliseconds
-            // TODO: This variable is probably not necessary any more
-            $scope.firstData = true; // track whether this is the first data packet we have received
-				     // for purposes of not using an invalid dt value on first calculation
             // Target rotation values for the rocket model
             $scope.targetRotX = 0;
             $scope.targetRotY = 0;
@@ -163,7 +156,6 @@ angular.module("rvtk").directive("attitude", function() {
             // Gets called recursively with the requestAnimationFrame function call
             $scope.render = function () {
                 setTimeout(function() { // setTimeout used to run at 30 fps
-                    // TODO: should be requestAnimationFrame($scope.render);?
                     requestAnimationFrame($scope.render);
 
                     // Calculate difference in rotations
@@ -191,141 +183,6 @@ angular.module("rvtk").directive("attitude", function() {
             // The socket.io function at the beginning of the file will continuously receive and the data packets
             // so no need for the loop() function.
 
-            // For some reason the imuData variable seems to fall out of scope and become undefined after the
-            // call to process(). I don't know why this behaves this way, but having the rest of the code execute
-            // within a function that is called in the $.ajax(... section fixes this scoping issue.
-            /*
-            $scope.allThis = function (){
-
-                // TODO: According to Matt this may not be necessary. Consider removing this in the future
-                // Resize the $scope.renderer when the window resizes.
-                function onWindowResize(event) {
-                    $scope.renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
-                }
-
-                // Render the scene, and change the rocket's attitude at 30fps
-                // Gets called recursively with the requestAnimationFrame function call
-                $scope.render = function () {
-                    setTimeout(function() {	// setTimeout used to run at 30 fps
-                        requestAnimationFrame(render);
-		    
-                        // Calculate difference in rotations
-                        $scope.diffX = $scope.targetRotX - $scope.prevRotX;
-                        $scope.diffY = $scope.targetRotY - $scope.prevRotY;
-                        $scope.diffZ = $scope.targetRotZ - $scope.prevRotZ;
-
-                        // Update previous rotation
-                        $scope.prevRotX = $scope.targetRotX;
-                        $scope.prevRotY = $scope.targetRotY;
-                        $scope.prevRotZ = $scope.targetRotZ;
-	
-                        // Rotate the model
-                        $scope.mesh.rotateX($scope.diffX);
-                        $scope.mesh.rotateY($scope.diffY);
-                        $scope.mesh.rotateZ($scope.diffZ);
-
-                        // Three.js function call to render the scene
-                        $scope.renderer.render($scope.scene, $scope.camera);
-                    }, 1000/30);	// run at 30fps
-                }
-
-                // This function will process a data packet and update the three$scope.targetRot variables
-                // Uses a complementary filter between the gyro and accelerometer data to ensure accuracy
-                // and avoid drift errors.
-                $scope.calculateData = function (){
-                    // Grab current line of IMU data
-                    //var x = imuData[i];
-
-                    // Variables for calculating the pitch and yaw of the rocket from the accelerometer data
-                    $scope.accPitch = 0;
-                    $scope.accYaw = 0;
-                
-                    //$scope.deltaTime = $scope.time[i] - scope.time[i - 1];
-                    $scope.deltaTime = $scope.PreviousTimeStamp - $scope.TimeStamp;
-                    $scope.deltaTime = $scope.dt * Math.pow(10, -9); // need to convert nanoseconds to seconds
-
-
-                    // Gyroscope data is in degrees/second. Need to convert to radians.
-                    // Data is an angular displacement instead of an absolute angle, so data is
-                    // added to target rotation instead of assigning the rotation.
-                    // For some reason it appears that the y axis values are actually in the x axis column
-                    // so I am switching them here. 
-                    // TODO: Currently do not know if x is really x axis or z axis because of the y values
-                    // being in the x column. Need to investigate further. Currently looks fine 
-                    //targetRotX += Math.radians(x[1]) * dt;
-                   $scope.targetRotX += Math.radians($scope.Gyro_Y);
-                    //targetRotY += Math.radians(x[0]) * dt;
-                   $scope.targetRotY += Math.radians($scope.Gyro_X) * dt;
-                    //targetRotZ += Math.radians(x[2]) * dt;
-                   $scope.targetRotZ += Math.radians($scope.Gyro_Z) * dt;
-
-                    // Uses data from the accelerometer to obtain a pitch and yaw angle. Note that accelerometer
-                    // cannot track roll, so only calculating the pitch and yaw.
-                    //$scope.accPitch = Math.atan2(x[5], x[3]);
-                    $scope.accPitch = Math.atan2($scope.Acc_Z, $scope.Acc_X);
-                    //$scope.accYaw = Math.atan2(x[4], x[3]);
-                    $scope.accYaw = Math.atan2($scope.Acc_Y, $scope.Acc_X);
-
-
-                    //var $scope.accMagn = Math.pow(x[3], 2) + Math.pow(x[4], 2) + Math.pow(x[5], 2);
-                    $scope.accMagn = Math.pow($scope.Acc_X, 2) + Math.pow($scope.Acc_Y, 2) + Math.pow($scope.Acc_Z, 2);
-
-                    // Combine the gyroscope data with the accelerometer data to ensure that the orientation is both
-                    // accurate and free of any drift errors that are present with using gyroscopes.
-                    // TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis. 
-                    // This would get fixed by using magnometer data, however, initial search into these calculations
-                    // seems exceedingly complex and I am not if it is worth the effort. 
-
-                    // A check to factor in the accelerometer data when only within a certain range. If the data is too large
-                    // or too small it is not reliable since accelerometers are susceptible to outside forces which 
-                    // disturbs the data.
-                    if($scope.accMagn > 8000 && $scope.accMagn < 32000){
-                               $scope.targetRotX = (0.98 * $scope.targetRotX) + (0.02 * $scope.accPitch);
-                               $scope.targetRotZ = (0.98 * $scope.targetRotZ) + (0.02 * $scope.accYaw);
-                    }
-                }
-            
-	            // Function which is used to loop through the line of data in the ADIS.csv file (the contents of which
-                // are now currently in imuData with i being the current index for what line we are currently on).
-                // This function was added so that this program can go through data packets at the rate that the packets
-	            // came in at, so that its rate is independant of how often the model is being rendered.
-	            // TODO: This function will need to be changed in the future to receive data packets from the server instead
-	            // of looping through a file. Also should get rid of the recursive function call as well to avoid stack overflow.
-                /*
-                function loop(){
-                    setTimeout(function(){
-			            // Process a line of data
-                        $scope.calculateData();
-			
-			            // If it is the beginning of the log, set model's orientation to 0.
-                        if(firstData == true){
-			                // TODO: This check should be used to initialize the target rotation with accelerometer data.
-                            firstData = false;
-                        }
-			            // If it isn't the beginning of the log, calculate new dt
-                        else{
-                        }
-
-			            // Increment the file index variable
-                        i += 1;
-
-                        if(i == imuData.length - 1){ // in order to loop when at the end of the ADIS.csv file
-                            i = 0;
-                            firstData = true;
-                        }
-
-                        $scope.loopDelay = dt * Math.pow(10, 3); // convert seconds to milliseconds since delay time is in milliseconds
-                        loop();
-                    }, $scope.loopDelay);	// loopDelay is calculated by the time between current data packet and last data packet.
-	   				// This is done to try to simulate the rate at which the data packets came in during the live launch
-                }
-
-		        // Call loop and render to process the data and render the scene
-                //loop();
-                render();
-
-	    }
-        */
         }],
         templateUrl: ''
     };
