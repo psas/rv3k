017f09c98b6599500d5a8456e14625b2d415bdc8
mikeohl <mohl@pdx.edu>
2017-05-27 06:59:46 -0700

    Updated earthFrameView with configuration, rocket model, and reset function.

--- front-end/app/directives/earthFrameView.js
+++ front-end/app/directives/earthFrameView.js
@@ -1,63 +1,110 @@
 /**
- * This file includes all the logic for the earth frame view. It works in conjuction with the earthFrameView.html page to render a 3D map that and objects on that map.
+ * earthFrameView.js includes all the logic for the earth frame view. It works in conjuction with the earthFrameView.html page to render a 3D map that and objects on that map.
+ * Copyright (C) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva, Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen
+ * Michael Ohl, Mathew Tighe
  *
- * Copyright (c) 2017 Jeff Patterson, Amanda Murphy, Paolo Villanueva, Patrick Overton, Connor Picken, Yun Cong Chen, Seth Amundsen, Michael Ohl, Matthew Tighe
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either vesion 2 of the License, or (at our option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY of FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
  * ALL RIGHTS RESERVED
- * [This program is licensed under the "GNU General Public License"]
+ * This program is licensed under the "GNU General Public License"
  * Please see the file COPYING in the source
  * distribution of this software for license terms.
  */
 
-'use strict'
+"use strict"
 
-angular.module("rvtk").directive("earthFrameView", function() {
+app.directive("earthFrameView", function() {
     return {
-        restrict: 'E',
+        restrict: "E",
         scope: {},
-        controller: ['$scope', function earthFrameViewController($scope) {
-            // -----------------------------
-            // all 3D map stuff
-            // creates the 3D map viewer
-            var viewer = new Cesium.Viewer('cesiumContainer', {
-                // gets rid of a menu to pick map type
-                baseLayerPicker : false,
-                // hides clock, timeline bar, and home button
-                timeline: false,
-                animation: false,
-                homeButton: false,
-                terrainExaggeration : 2.0
-            });
-
-            // set lighting to true - makes it look more realistic
-            viewer.scene.globe.enableLighting = true;
-
-            viewer.terrainProvider = new Cesium.CesiumTerrainProvider({
-                url : 'https://assets.agi.com/stk-terrain/world',
-                requestWaterMask : true,
-                requestVertexNormals : true
-            });
-
-
-            $scope.RocketPosition = Cesium.Cartesian3.fromDegrees(-120.6517673, 43.7961328, 3000);
-
-            // Make a dot for the rocket
-            var rocket = viewer.entities.add({
-                name : 'Rocket',
-                position: $scope.RocketPosition,
-                ellipsoid : {
-                    radii : new Cesium.Cartesian3(300.0, 300.0, 300.0),
-                    material : Cesium.Color.BLUE,
-                    outline : false
-                }
-
-            });
+        controller: ['$scope', 'config', function earthFrameViewController($scope, config) {
 
             // Radius of recovery crew
-            $scope.radius  = 3000.0;
+            $scope.radius = config.recoveryCrewSize;
             $scope.recoveryCrews = {};
 
+            // the array of longitude, latitude and altitude points sent in from the back-end
+            var trajectoryPoints = [];
+
+            // The point on the ground under the rocket
+            var groundPos = Cesium.Cartesian3.fromDegrees(config.launchLocation.longitude, config.launchLocation.latitude);
+
+            // var terrainProvider = new Cesium.CesiumTerrainProvider({
+            //     url : 'https://assets.agi.com/stk-terrain/v1/tilesets/world/tiles',
+            //     requestWaterMask : true,
+            //     requestVertexNormals : true
+            // });
+
+            // var imageryProvider = Cesium.createTileMapServiceImageryProvider({
+            //    url : '/bower_components/cesium.js/dist/Assets/Textures/NaturalEarthII',
+            //    fileExtension : 'jpg'
+            // });
+
+            // Initializes the Cesium Viewer, positions the camera, and adds entities
+            $scope.init = function () {
+                // Create the viewer
+                $scope.viewer = new Cesium.Viewer('cesiumContainer', {
+                    // imageryProvider: imageryProvider,    // Activate imagery overlay
+                    // terrainProvider: terrainProvider,    // Activate terrain
+                    baseLayerPicker: false,     // disables menu to choose map type
+                    homeButton: false,          // disables home button
+                    timeline: false,            // disables timeline
+                    animation: false,           // disables clock
+                    fullscreenButton: false,    // fullscreen doesn't work correctly
+                    geocoder: false,            // disables landmark detection
+                    selectionIndicator: false,  // disables indicator on selected object
+                    sceneModePicker: false      // disables default scene picker
+                });
+
+                // Activate directional lighting for terrain (globe will be dark at night)
+                // $scope.viewer.scene.globe.enableLighting = true;
+
+                // Set Initial Rocket Position
+                $scope.RocketPosition = Cesium.Cartesian3.fromDegrees(config.launchLocation.longitude, config.launchLocation.latitude, config.launchLocation.height);
+
+                // Make a dot for the rocket
+                var rocket = $scope.viewer.entities.add({
+                    name : 'Rocket',
+                    position: $scope.RocketPosition,
+                    model : {
+                        uri : 'assets/rocket.gltf',
+                        color : Cesium.Color.BLUE,
+                        minimumPixelSize : config.rocketSize,
+                        maximumScale : 1000,
+                        colorBlendAmount : 1.0
+                    }
+                });
+                $scope.viewer.trackedEntity = rocket;
+
+                // Positions the camera so that all entities are in view
+                $scope.viewer.zoomTo($scope.viewer.entities);
+
+                // Position the camera gradually with flyTo
+                // $scope.viewer.camera.flyTo({
+                //     destination: new Cesium.Cartesian3.fromDegrees(config.launchLocation.longitude, config.launchLocation.latitude, 1500)
+                // });
+
+                // Gets rid of a developer tools error and allows cesium to work
+                $scope.viewer.infoBox.frame.sandbox = "allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts";
+
+                // The projected trajectory of the rocket.
+                var projTraj = $scope.viewer.entities.add({
+                    name : "Projected Trajectory",
+                    polyline : {
+                        positions : [$scope.RocketPosition, groundPos],
+                            width : 3,
+                        material : Cesium.Color.WHITE
+                    }
+                });
+            };
+
+            // Adds Recovery Crew Dots to globe
             var addDotForRecoveryCrew = function(callsign, lat, longi) {
-                var recoveryCrew = viewer.entities.add({
+                var recoveryCrew = $scope.viewer.entities.add({
                     name : callsign,
                     position: Cesium.Cartesian3.fromDegrees(longi, lat),
                     point : {
@@ -77,33 +124,11 @@ angular.module("rvtk").directive("earthFrameView", function() {
                 });
                 $scope.recoveryCrews[callsign] = recoveryCrew;
             }
-            
 
-            // function to move recovery crew 1's longitude and latitude +1 degree
+            // Move recovery crew
             $scope.movePoints = function(callsign, lat, longi) {
                 $scope.recoveryCrews[callsign].position = Cesium.Cartesian3.fromDegrees(longi, lat);
             };
-
-            // positions the camera so that all entities are in view
-            viewer.zoomTo(viewer.entities);
-
-            // Gets rid of a developer tools error and allows cesium to work
-            viewer.infoBox.frame.sandbox = "allow-same-origin allow-top-navigation allow-pointer-lock allow-popups allow-forms allow-scripts";
-
-            // the array of longitude, latitude and altitude points sent in from the back-end
-            var trajectoryPoints = [];
-
-            // The point on the ground under the rocket
-            var groundPos = Cesium.Cartesian3.fromDegrees(-120.6517673, 43.7961328);
-            // The projected trajectory of the rocket.
-            var projTraj = viewer.entities.add({
-                name : "Projected Trajectory",
-                polyline : {
-                    positions : [$scope.RocketPosition, groundPos],
-                        width : 3,
-                    material : Cesium.Color.WHITE
-                }
-            });
 
             // helper function, used to update the rockets position
             $scope.moveRocket = function(x, y, z) {
@@ -116,7 +141,7 @@ angular.module("rvtk").directive("earthFrameView", function() {
                 // move the rocket to new position
                 rocket.position = $scope.RocketPosition;
                 // Declare the trajectory line
-                var actualTrajectory = viewer.entities.add({
+                var actualTrajectory = $scope.viewer.entities.add({
                         name : 'Actual Trajectory',
                         polyline : {
                             positions : Cesium.Cartesian3.fromDegreesArrayHeights(trajectoryPoints),
@@ -134,21 +159,32 @@ angular.module("rvtk").directive("earthFrameView", function() {
                 }
             }
 
-            // Changes the camera location and tilt when a new item is selected in the efv 
+            // Changes the camera location and tilt when a new item is selected in the efv
             // camera drop down menu
             $scope.changeCameraView = function() {
                 var viewKey = $scope.selected;
                 switch(viewKey) {
                     case 'side':
                         var offset = new Cesium.Cartesian3(70642.66030209465, -31661.517948317807, 35505.179997143336);
-                        viewer.camera.lookAt($scope.RocketPosition, offset);
+                        $scope.viewer.camera.lookAt($scope.RocketPosition, offset);
                         break;
                     case 'top':
-                        viewer.zoomTo(viewer.entities);
+                        $scope.viewer.zoomTo($scope.viewer.entities);
                         break;
                 }
-
             };
+
+            // Initialize Cesium
+            $scope.init();
+
+            // Reset Cesium Viewer
+            $scope.reset = function () {
+                $scope.viewer.destroy();
+                console.log("Cesium Reset");
+                if ($scope.viewer.isDestroyed()) {
+                    $scope.init();
+                }
+            }
 
             var namespace = '/main';
             // this port connects to port broadcast by ../unified/app.py
@@ -158,19 +194,18 @@ angular.module("rvtk").directive("earthFrameView", function() {
 
             telemetrySocket.on('telemetry', function(data) {
                 for(var key in data) {
-                    if(key == 'V8A8') {
+                    if(key == config.V8A8) {
                         //update anything that uses V8A8 data in cesium
                         $scope.RocketPosition = Cesium.Cartesian3.fromDegrees(data[key].Longitude, data[key].Latitude, data[key].MSL_Altitude);
-                        $scope.moveRocket(data[key].Longitude, data[key].Latitude, data[key].MSL_Altitude);                    
+                        $scope.moveRocket(data[key].Longitude, data[key].Latitude, data[key].MSL_Altitude);
                     }
                 }
-
             });
 
             var aprsSocket = io.connect('http://' + document.domain + ':8081' + namespace);
             aprsSocket.on('connect', function() {});
             aprsSocket.on('disconnect', function() {});
-    
+
             aprsSocket.on('recovery', function(data) {
                 console.log(data);
                 var callsign = data["callsign"];
@@ -182,10 +217,7 @@ angular.module("rvtk").directive("earthFrameView", function() {
                 } else {
                     $scope.movePoints(callsign, lat, longi);
                 }
-
             });
-
-
 
         }],
         templateUrl: 'directives/earthFrameView.html'
