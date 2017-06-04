9ba082530c14a938a7766485896b565ce1c64203
Connor Picken <cpicken@pdx.edu>
2017-06-04 00:27:50 -0700

    Moved changes from previous attitude branch to current branch.

--- front-end/app/directives/attitude.js
+++ front-end/app/directives/attitude.js
@@ -15,41 +15,61 @@
  * distribution of this software for license terms.
  */
 
-"use strict";
+'use strict';
 
-app.directive("attitude", function() {
+angular.module("rvtk").directive("attitude", function() {
     return {
         restrict: 'E',
         scope: {},
-        controller: ['$scope', '$element', 'config', function attitudeController($scope, $element, config) {
+        controller: ['$scope', '$element', function attitudeController($scope, $element) {
 
             var namespace = '/main';
             // this port connects to port broadcast by ../unified/app.py
-            var socket = io.connect('http://' + config.serverSource + ':8080' + namespace);
+            var socket = io.connect('http://' + document.domain + ':8080' + namespace);
             socket.on('connect', function() {});
             socket.on('disconnect', function() {});
-
+            
             socket.on('telemetry', function(data) {
                 for(var key in data) {
-                    if(key == config.ADIS) {
+                    if(key == 'ADIS') {
                         $scope.Gyro_X = data[key][config.Gyro_X];
                         $scope.Gyro_Y = data[key][config.Gyro_Y];
                         $scope.Gyro_Z = data[key][config.Gyro_Z];
                         $scope.Acc_X = data[key][config.Acc_X];
                         $scope.Acc_Y = data[key][config.Acc_Y];
                         $scope.Acc_Z = data[key][config.Acc_Z];
-
                         // If TimeStamp has not been initialized we set previous to current,
                         // deltaTime will be 0 for very first calculateData call.
                         if($scope.TimeStamp) {
                             $scope.PreviousTimeStamp = $scope.TimeStamp;
-                        }
+                        } 
                         else {
-                            $scope.PreviousTimeStamp = data[key][config.timestamp];
+                            $scope.PreviousTimeStamp = data[key].timestamp;
                         }
 
-                        $scope.TimeStamp = data[key][config.timestamp];
+                        $scope.TimeStamp = data[key].timestamp;
                         $scope.calculateData();
+
+                    }
+                    // If check to reset the rocket when looping the replay log. Should be removed in final product
+                    else if(key == 'SEQN'){
+                        if(data[key].Sequence == 93251 || data[key].Sequence < 90510){ 
+                            $scope.mesh.rotation.x = 0;
+                            $scope.mesh.rotation.y = 0;
+                            $scope.mesh.rotation.z = 0;
+
+                            $scope.targetRotX = 0;
+                            $scope.targetRotY = 0;
+                            $scope.targetRotZ = 0;
+            
+                            $scope.prevRotX = 0;
+                            $scope.prevRotY = 0;
+                            $scope.prevRotZ = 0;
+
+                            $scope.diffX = 0;
+                            $scope.diffY = 0;
+                            $scope.diffZ = 0;
+                        }
                     }
                 }
             });
@@ -59,65 +79,85 @@ app.directive("attitude", function() {
                 return degrees * Math.PI / 180;
             }
 
-            $scope.loader1 = new THREE.JSONLoader();
-
+            $scope.loaderJSON = new THREE.JSONLoader();
+            // Use this load function when using an export option directly to Three.js format
             // Load the rocket model into the $scope.mesh variable.
-            // The inner function is not called until loader1.load completes.
-            $scope.loadModel = function (modelUrl) {
-                $scope.loader1.load(modelUrl, function(geometry, materials) {
+            // The inner function is not called until loaderJSON.load completes.
+            $scope.loadModelJSON = function (modelUrl) {
+                $scope.loaderJSON.load(modelUrl, function(geometry, materials) {
                     $scope.material = new THREE.MeshFaceMaterial(materials);
                     $scope.mesh = new THREE.Mesh(geometry, $scope.material);
+                    // Move rocket slightly up so not colliding with the background x & z axes
                     $scope.mesh.position.y += 5;
                     $scope.scene.add($scope.mesh);
-
-                    // need to update camera's rotation here since the lookAt target only exists once
+                     
+                    // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
                     // the rocket mesh has been created
                     $scope.camera.lookAt($scope.mesh.position);
-                    // point the camera up a little bit, otherwise the camera looks too far down on the rocket
+                    // Point the camera up a little bit, otherwise the camera looks too far down on 
                     $scope.camera.rotateX(0.16);
                 });
             }
 
+            $scope.loaderMTL = new THREE.MTLLoader();
+            // Use this load function when using an export option that gives an .obj and .mtl file
+            // Load the rocket model into the $scope.mesh variable
+            // The inner function is not called until loadOBJ.load completes
+            $scope.loadModelOBJ = function(modelUrl, mtlUrl){
+                $scope.loaderMTL.load(mtlUrl, function(materials){
+                    materials.side = THREE.DoubleSide;
+                    materials.preload();
+                    $scope.loaderOBJ = new THREE.OBJLoader();
+                    $scope.loaderOBJ.setMaterials(materials);
+                    $scope.loaderOBJ.load(modelUrl, function(obj){
+                        $scope.mesh = obj;
+                        // Move rocket slightly up so not colliding with the background x & z axes
+                        $scope.mesh.position.y += 5;
+                        $scope.scene.add($scope.mesh);
+            
+                        // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
+                        // the rocket mesh has been created            
+                        $scope.camera.lookAt($scope.mesh.position);
+                        // Point the camera up a little bit, otherwise the camera looks too far down
+                        $scope.camera.rotateX(0.16);
+                    });
+                });
+            };
+
+            // Use this load function when using an export option that gives only an .obj file
+            // Load the rocket model into the $scope.mesh variable
+            // The inner function is not called until loadOBJ.load completes
+            $scope.loadModelOBJOnly = function(modelUrl){
+                $scope.loaderOBJ = new THREE.OBJLoader();
+                $scope.loaderOBJ.load(modelUrl, function(obj){
+                    $scope.mesh = obj;
+                    // Move rocket slighty up so not colliding with the background x & z axes
+                    $scope.mesh.position.y += 5;
+                    $scope.scene.add($scope.mesh);
+                    
+                    // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
+                    // the rocket mesh has been created
+                    $scope.camera.lookAt($scope.mesh.position);
+                    // Point the camera up a little bit, otherwise the camera looks too far down
+                    $scope.camera.rotateX(0.16);
+                });
+            };
+
             // Set up the $scope.camera and $scope.renderer and attach them to html.
             // Start a listener for window resizing.
             $scope.init = function () {
-                $scope.canvas = document.getElementById('attitude-canvas');
-
-                // create scene and scene size
+                $scope.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, .1, 2000);
+                $scope.camera.position.set(-16, 10, 16); //original: (-14, 10, 22)
                 $scope.scene = new THREE.Scene();
-
-                // create the renderer and add it to the DOM
-                $scope.renderer = new THREE.WebGLRenderer({canvas: $scope.canvas, antialias: true, alpha: true});
-                $scope.canvas.width = $scope.canvas.clientWidth;
-                $scope.canvas.height = $scope.canvas.clientHeight*5;
-                $scope.renderer.setViewport(0,0,$scope.canvas.width, $scope.canvas.height);
-                // $element[0].appendChild($scope.renderer.domElement);
-
-                // create the camera with FOV, aspect ratio, distance from scene, clipping distance
-                $scope.camera = new THREE.PerspectiveCamera(config.FOV, $scope.canvas.width / $scope.canvas.height, .1, 2000);
-                $scope.camera.position.set(-14, 10, 22); // pos x,y,z
-                $scope.scene.add($scope.camera);
-
-                // set the background color of the scene
-                $scope.renderer.setClearColor(new THREE.Color(0xffffff, 1));
+                $scope.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+                $scope.renderer.setSize(window.innerWidth / 2, window.innerHeight / 1.5);     
 
                 // set up light for the scene
                 $scope.light = new THREE.DirectionalLight(0xffffff, 1);
                 $scope.light.position.set(10, 50, 100);
                 $scope.scene.add($scope.light);
 
-                //$scope.attitudeGraphic = $scope.renderer.domElement;
-                //$scope.$apply();
-            }
-
-            $scope.resize = function () {
-                if ($scope.canvas.width != $scope.canvas.clientWidth || $scope.canvas.height != $scope.canvas.clientHeight) {
-                    $scope.canvas.width = $scope.canvas.clientWidth;
-                    $scope.canvas.height = $scope.canvas.clientHeight;
-                    $scope.renderer.setViewport(0,0,$scope.canvas.width, $scope.canvas.height);
-                    $scope.camera.aspect = $scope.canvas.width / $scope.canvas.height;
-                    $scope.camera.updateProjectionMatrix();
-                }
+                $element[0].appendChild($scope.renderer.domElement);
             }
 
             // Function which initializes the background. Adds a red and green line for the axises and sets the background to
@@ -125,7 +165,7 @@ app.directive("attitude", function() {
             $scope.bgInit = function(){
                 // Adds red line along the x axis
                 var lineGeometry = new THREE.Geometry();
-                lineGeometry.vertices.push(new THREE.Vector3(-800, 0, 0), new THREE.Vector3(800, 0, 0));
+                lineGeometry.vertices.push(new THREE.Vector3(-5000, 0, 0), new THREE.Vector3(5000, 0, 0));
                 lineGeometry.computeLineDistances();
                 var lineMaterial = new THREE.LineBasicMaterial({color: 0xff0000, linewidth: 2});
                 var line = new THREE.Line(lineGeometry, lineMaterial);
@@ -133,14 +173,17 @@ app.directive("attitude", function() {
 
                 // Adds green line along the z axis
                 var lineGeometry = new THREE.Geometry();
-                lineGeometry.vertices.push(new THREE.Vector3(0, 0, -800), new THREE.Vector3(0, 0, 800));
+                lineGeometry.vertices.push(new THREE.Vector3(0, 0, -5000), new THREE.Vector3(0, 0, 5000));
                 lineGeometry.computeLineDistances();
                 var lineMaterial = new THREE.LineBasicMaterial({color: 0x00ff00, linewidth: 2});
                 var line = new THREE.Line(lineGeometry, lineMaterial);
                 $scope.scene.add(line);
 
                 // Set background color to a nice neutral color with an opacity of 1
-                $scope.renderer.setClearColor(0x355565, 1);
+                //$scope.renderer.setClearColor(0x87CEEB, 1);
+                $scope.renderer.setClearColor(0xB0E0E6, 1);
+                //$scope.renderer.setClearColor(0xAFEEEE, 1);
+                //$scope.renderer.setClearColor(0x483D8B, 1);
             }
 
             // This function will process a data packet and update the three $scope.targetRot variables
@@ -150,7 +193,7 @@ app.directive("attitude", function() {
                 // Variables for calculating the pitch and yaw of the rocket from the accelerometer data
                 $scope.accPitch = 0;
                 $scope.accYaw = 0;
-
+                
                 //$scope.deltaTime = $scope.time[i] - scope.time[i - 1];
                 $scope.deltaTime = $scope.TimeStamp - $scope.PreviousTimeStamp;
                 $scope.deltaTime = $scope.deltaTime * Math.pow(10, -9); // need to convert nanoseconds to seconds
@@ -159,9 +202,9 @@ app.directive("attitude", function() {
                 // Data is an angular displacement instead of an absolute angle, so data is
                 // added to target rotation instead of assigning the rotation.
                 // For some reason it appears that the y axis values are actually in the x axis column
-                // so I am switching them here.
+                // so I am switching them here. 
                 // TODO: Currently do not know if x is really x axis or z axis because of the y values
-                // being in the x column. Need to investigate further. Currently looks fine
+                // being in the x column. Need to investigate further. Currently looks fine 
                 //targetRotX += Math.radians(x[1]) * dt;
                 $scope.targetRotX += Math.radians($scope.Gyro_Y) * $scope.deltaTime;
                 //targetRotY += Math.radians(x[0]) * dt;
@@ -171,28 +214,27 @@ app.directive("attitude", function() {
 
                 // Uses data from the accelerometer to obtain a pitch and yaw angle. Note that accelerometer
                 // cannot track roll, so only calculating the pitch and yaw.
-                //$scope.accPitch = Math.atan2(x[5], x[3]);
                 $scope.accPitch = Math.atan2($scope.Acc_Z, $scope.Acc_X);
-                //$scope.accYaw = Math.atan2(x[4], x[3]);
                 $scope.accYaw = Math.atan2($scope.Acc_Y, $scope.Acc_X);
 
 
-                //var $scope.accMagn = Math.pow(x[3], 2) + Math.pow(x[4], 2) + Math.pow(x[5], 2);
                 $scope.accMagn = Math.pow($scope.Acc_X, 2) + Math.pow($scope.Acc_Y, 2) + Math.pow($scope.Acc_Z, 2);
 
                 // Combine the gyroscope data with the accelerometer data to ensure that the orientation is both
                 // accurate and free of any drift errors that are present with using gyroscopes.
-                // TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis.
-                // This would get fixed by using magnometer data, however, initial search into these calculations
-                // seems exceedingly complex and I am not if it is worth the effort.
 
                 // A check to factor in the accelerometer data when only within a certain range. If the data is too large
-                // or too small it is not reliable since accelerometers are susceptible to outside forces which
+                // or too small it is not reliable since accelerometers are susceptible to outside forces which 
                 // disturbs the data.
                 if($scope.accMagn > 8000 && $scope.accMagn < 32000){
+                    // Combining values with a simple complimentary filter
                     $scope.targetRotX = (0.98 * $scope.targetRotX) + (0.02 * $scope.accPitch);
                     $scope.targetRotZ = (0.98 * $scope.targetRotZ) + (0.02 * $scope.accYaw);
                 }
+
+                // TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis. 
+                // This would get fixed by using magnometer data, however, initial search into these calculations
+                // seems exceedingly complex and I am not if it is worth the effort. 
             }
 
             $scope.deltaTime = 0.0012;	// change in time between data packets
@@ -208,9 +250,9 @@ app.directive("attitude", function() {
             $scope.prevRotZ = $scope.targetRotZ;
 
             // Render the scene, and change the rocket's attitude at 30fps
-            // Gets called recursively with the requestAnimationFrame function call
+            // Gets called repeatedly with the requestAnimationFrame function call
             $scope.render = function () {
-                setTimeout(function() { // setTimeout used to run at 30 fps
+                setTimeout(function() { // setTimeout used to run at a consistent framerate
                     requestAnimationFrame($scope.render);
 
                     // Calculate difference in rotations
@@ -230,7 +272,6 @@ app.directive("attitude", function() {
 
                     // Three.js function call to render the scene
                     $scope.renderer.render($scope.scene, $scope.camera);
-                    $scope.resize();
                 }, 1000/30); // run at 30fps
             }
 
@@ -238,8 +279,23 @@ app.directive("attitude", function() {
             $scope.init();
             // Create the background
             $scope.bgInit();
-            // Create the rocket model
-            $scope.loadModel("assets/rocket_final_obj.js");
+
+            /* ----------------------------------------------------------------------------------------------
+             * Choose load function based on what file types are available.
+             * Load in custom/future models by placing the three.js .js file, .obj & .mtl files, or .obj file
+             * into assets directory located in the app directory. Then update the file path located in the
+             * arguments section of the appropriate load function.
+             * ----------------------------------------------------------------------------------------------*/
+
+            // Create the rocket model using a three.js .js file
+            //$scope.loadModelJSON("assets/rocket_final_obj.js");
+
+            // Create the rocket model using .obj and .mtl files
+            $scope.loadModelOBJ("assets/rocket.obj", "assets/rocket.mtl");
+
+            // Create the rocket model using only a .obj file
+            //$scope.loadModelOBJOnly("assets/rocket.obj");
+
             // Render will call it self 30 times a second to render the scene and update the rocket's rotation
             $scope.render();
         }],
