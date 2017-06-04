df9e2a4b367200f0aa527ecd79b62b37e0e2467e
mikeohl <mohl@pdx.edu>
2017-06-04 14:51:13 -0700

    merged model changes with init and resize changes

--- front-end/app/directives/attitude.js
+++ front-end/app/directives/attitude.js
@@ -17,21 +17,21 @@
 
 'use strict';
 
-angular.module("rvtk").directive("attitude", function() {
+app.directive("attitude", function() {
     return {
         restrict: 'E',
         scope: {},
-        controller: ['$scope', '$element', function attitudeController($scope, $element) {
+        controller: ['$scope', '$element', 'config', function attitudeController($scope, $element, config) {
 
             var namespace = '/main';
             // this port connects to port broadcast by ../unified/app.py
             var socket = io.connect('http://' + document.domain + ':8080' + namespace);
             socket.on('connect', function() {});
             socket.on('disconnect', function() {});
-            
+
             socket.on('telemetry', function(data) {
                 for(var key in data) {
-                    if(key == 'ADIS') {
+                    if(key == config.ADIS) {
                         $scope.Gyro_X = data[key][config.Gyro_X];
                         $scope.Gyro_Y = data[key][config.Gyro_Y];
                         $scope.Gyro_Z = data[key][config.Gyro_Z];
@@ -42,18 +42,18 @@ angular.module("rvtk").directive("attitude", function() {
                         // deltaTime will be 0 for very first calculateData call.
                         if($scope.TimeStamp) {
                             $scope.PreviousTimeStamp = $scope.TimeStamp;
-                        } 
+                        }
                         else {
-                            $scope.PreviousTimeStamp = data[key].timestamp;
+                            $scope.PreviousTimeStamp = data[key][config.timestamp];
                         }
 
-                        $scope.TimeStamp = data[key].timestamp;
+                        $scope.TimeStamp = data[key][config.timestamp];
                         $scope.calculateData();
 
                     }
                     // If check to reset the rocket when looping the replay log. Should be removed in final product
-                    else if(key == 'SEQN'){
-                        if(data[key].Sequence == 93251 || data[key].Sequence < 90510){ 
+                    else if(key == config.SEQN){
+                        if(data[key][config.Sequence] == 93251 || data[key][config.Sequence] < 90510){
                             $scope.mesh.rotation.x = 0;
                             $scope.mesh.rotation.y = 0;
                             $scope.mesh.rotation.z = 0;
@@ -61,7 +61,7 @@ angular.module("rvtk").directive("attitude", function() {
                             $scope.targetRotX = 0;
                             $scope.targetRotY = 0;
                             $scope.targetRotZ = 0;
-            
+
                             $scope.prevRotX = 0;
                             $scope.prevRotY = 0;
                             $scope.prevRotZ = 0;
@@ -90,11 +90,11 @@ angular.module("rvtk").directive("attitude", function() {
                     // Move rocket slightly up so not colliding with the background x & z axes
                     $scope.mesh.position.y += 5;
                     $scope.scene.add($scope.mesh);
-                     
+
                     // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
                     // the rocket mesh has been created
                     $scope.camera.lookAt($scope.mesh.position);
-                    // Point the camera up a little bit, otherwise the camera looks too far down on 
+                    // Point the camera up a little bit, otherwise the camera looks too far down on
                     $scope.camera.rotateX(0.16);
                 });
             }
@@ -114,9 +114,9 @@ angular.module("rvtk").directive("attitude", function() {
                         // Move rocket slightly up so not colliding with the background x & z axes
                         $scope.mesh.position.y += 5;
                         $scope.scene.add($scope.mesh);
-            
+
                         // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
-                        // the rocket mesh has been created            
+                        // the rocket mesh has been created
                         $scope.camera.lookAt($scope.mesh.position);
                         // Point the camera up a little bit, otherwise the camera looks too far down
                         $scope.camera.rotateX(0.16);
@@ -134,7 +134,7 @@ angular.module("rvtk").directive("attitude", function() {
                     // Move rocket slighty up so not colliding with the background x & z axes
                     $scope.mesh.position.y += 5;
                     $scope.scene.add($scope.mesh);
-                    
+
                     // Need to update camera's rotation here since the lookAt target (the rocket) only exists once
                     // the rocket mesh has been created
                     $scope.camera.lookAt($scope.mesh.position);
@@ -146,18 +146,40 @@ angular.module("rvtk").directive("attitude", function() {
             // Set up the $scope.camera and $scope.renderer and attach them to html.
             // Start a listener for window resizing.
             $scope.init = function () {
-                $scope.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, .1, 2000);
-                $scope.camera.position.set(-16, 10, 16); //original: (-14, 10, 22)
+                $scope.canvas = document.getElementById('attitude-canvas');
+
+                // create scene and scene size
                 $scope.scene = new THREE.Scene();
-                $scope.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
-                $scope.renderer.setSize(window.innerWidth / 2, window.innerHeight / 1.5);     
+
+                // create the renderer and add it to the DOM
+                $scope.renderer = new THREE.WebGLRenderer({canvas: $scope.canvas, antialias: true, alpha: true});
+                $scope.canvas.width = $scope.canvas.clientWidth;
+                $scope.canvas.height = $scope.canvas.clientHeight*5;
+                $scope.renderer.setViewport(0,0,$scope.canvas.width, $scope.canvas.height);
+                // $element[0].appendChild($scope.renderer.domElement);
+
+                // create the camera with FOV, aspect ratio, distance from scene, clipping distance
+                $scope.camera = new THREE.PerspectiveCamera(config.FOV, $scope.canvas.width / $scope.canvas.height, .1, 2000);
+                $scope.camera.position.set(-16, 10, 16); // pos x,y,z
+                $scope.scene.add($scope.camera);
+
+                // set the background color of the scene
+                $scope.renderer.setClearColor(new THREE.Color(0xffffff, 1));
 
                 // set up light for the scene
                 $scope.light = new THREE.DirectionalLight(0xffffff, 1);
                 $scope.light.position.set(10, 50, 100);
                 $scope.scene.add($scope.light);
+            }
 
-                $element[0].appendChild($scope.renderer.domElement);
+            $scope.resize = function () {
+                if ($scope.canvas.width != $scope.canvas.clientWidth || $scope.canvas.height != $scope.canvas.clientHeight) {
+                    $scope.canvas.width = $scope.canvas.clientWidth;
+                    $scope.canvas.height = $scope.canvas.clientHeight;
+                    $scope.renderer.setViewport(0,0,$scope.canvas.width, $scope.canvas.height);
+                    $scope.camera.aspect = $scope.canvas.width / $scope.canvas.height;
+                    $scope.camera.updateProjectionMatrix();
+                }
             }
 
             // Function which initializes the background. Adds a red and green line for the axises and sets the background to
@@ -193,7 +215,7 @@ angular.module("rvtk").directive("attitude", function() {
                 // Variables for calculating the pitch and yaw of the rocket from the accelerometer data
                 $scope.accPitch = 0;
                 $scope.accYaw = 0;
-                
+
                 //$scope.deltaTime = $scope.time[i] - scope.time[i - 1];
                 $scope.deltaTime = $scope.TimeStamp - $scope.PreviousTimeStamp;
                 $scope.deltaTime = $scope.deltaTime * Math.pow(10, -9); // need to convert nanoseconds to seconds
@@ -202,9 +224,9 @@ angular.module("rvtk").directive("attitude", function() {
                 // Data is an angular displacement instead of an absolute angle, so data is
                 // added to target rotation instead of assigning the rotation.
                 // For some reason it appears that the y axis values are actually in the x axis column
-                // so I am switching them here. 
+                // so I am switching them here.
                 // TODO: Currently do not know if x is really x axis or z axis because of the y values
-                // being in the x column. Need to investigate further. Currently looks fine 
+                // being in the x column. Need to investigate further. Currently looks fine
                 //targetRotX += Math.radians(x[1]) * dt;
                 $scope.targetRotX += Math.radians($scope.Gyro_Y) * $scope.deltaTime;
                 //targetRotY += Math.radians(x[0]) * dt;
@@ -224,7 +246,7 @@ angular.module("rvtk").directive("attitude", function() {
                 // accurate and free of any drift errors that are present with using gyroscopes.
 
                 // A check to factor in the accelerometer data when only within a certain range. If the data is too large
-                // or too small it is not reliable since accelerometers are susceptible to outside forces which 
+                // or too small it is not reliable since accelerometers are susceptible to outside forces which
                 // disturbs the data.
                 if($scope.accMagn > 8000 && $scope.accMagn < 32000){
                     // Combining values with a simple complimentary filter
@@ -232,9 +254,9 @@ angular.module("rvtk").directive("attitude", function() {
                     $scope.targetRotZ = (0.98 * $scope.targetRotZ) + (0.02 * $scope.accYaw);
                 }
 
-                // TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis. 
+                // TODO: currently only correcting drift in the pitch and yaw axises and not the roll axis.
                 // This would get fixed by using magnometer data, however, initial search into these calculations
-                // seems exceedingly complex and I am not if it is worth the effort. 
+                // seems exceedingly complex and I am not if it is worth the effort.
             }
 
             $scope.deltaTime = 0.0012;	// change in time between data packets
@@ -272,6 +294,7 @@ angular.module("rvtk").directive("attitude", function() {
 
                     // Three.js function call to render the scene
                     $scope.renderer.render($scope.scene, $scope.camera);
+                    $scope.resize();
                 }, 1000/30); // run at 30fps
             }
 
